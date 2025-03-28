919b5e5c50b925da6d47fc3e6eb62779e20b0e62
Ashley Hindle <hey@ashleyhindle.com>
2025-03-28 13:03:01 +0000

    adds initial classes

---
+++ src/MouseButton.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Whisp\Mouse;
+
+enum MouseButton: int
+{
+    case LEFT = 0;
+    case MIDDLE = 1;
+    case RIGHT = 2;
+    case RELEASED = 3;
+    case WHEEL_UP = 64;
+    case WHEEL_DOWN = 65;
+}
---
+++ src/MouseEvent.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Whisp\Mouse;
+
+class MouseEvent
+{
+    /** @var MouseMotion|MouseButton */
+    public function __construct(public $mouseEvent, public int $x = 0, public int $y = 0) {}
+}
